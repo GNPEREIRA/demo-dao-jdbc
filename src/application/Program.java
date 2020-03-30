13e0a6bd2c61273b0b0104f01794d1400de444dc
Gustavo Pereira <guspereira2010@gmail.com>
2020-03-30 15:40:46 -0300

    Seller Class e Program

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,14 +1,24 @@
 package application;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
 import entities.Department;
+import entities.Seller;
 
 public class Program {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ParseException {
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 
 		Department obj = new Department(1, "Books");
 		
 		System.out.println(obj);
+		
+		Seller obj1 = new Seller(1,"Gustavo", "gustavo@gmail.com", sdf.parse("24/03/1983"), 2000.0, obj);
+		
+		System.out.println(obj1);
 
 	}
 
--- src/entities/Seller.java
+++ src/entities/Seller.java
@@ -1,8 +1,13 @@
 package entities;
 
+import java.io.Serializable;
 import java.util.Date;
 
-public class Seller {
+public class Seller implements Serializable{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	private Integer id;
 	private String name;
 	private String email;
@@ -14,12 +19,13 @@ public class Seller {
 	public Seller() {
 	}
 	
-	public Seller(Integer id, String name, String email, Date birthDate, Double baseSalary) {
+	public Seller(Integer id, String name, String email, Date birthDate, Double baseSalary, Department department) {
 		this.id = id;
 		this.name = name;
 		this.email = email;
 		this.birthDate = birthDate;
 		this.baseSalary = baseSalary;
+		this.department = department;
 	}
 	
 	//get
@@ -43,6 +49,10 @@ public class Seller {
 		return baseSalary;
 	}
 	
+	public Department getDepartment() {
+		return department;
+	}
+	
 	//set
 	public void setId(Integer id) {
 		this.id = id;
@@ -63,4 +73,47 @@ public class Seller {
 	public void setBaseSalary(Double baseSalary) {
 		this.baseSalary = baseSalary;
 	}
+	
+	public void setDepartment(Department department) {
+		this.department = department;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Seller other = (Seller) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Seller [id=" + id + ", name=" + name + ", email=" + email + ", birthDate=" + birthDate + ", baseSalary="
+				+ baseSalary + ", department=" + department + "]";
+	}
+	
+	
 }
