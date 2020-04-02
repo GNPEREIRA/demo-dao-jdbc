bedc08b729a461dcc4744338e88a7c86e43e84f6
Gustavo Pereira <guspereira2010@gmail.com>
2020-04-02 09:00:28 -0300

    FindById Implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,28 +1,20 @@
 package application;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-
-import entities.Department;
 import entities.Seller;
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
 
 public class Program {
 
-	public static void main(String[] args) throws ParseException {
+	public static void main(String[] args) {
 		
-		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-
-		Department obj = new Department(1, "Books");
-		
-		System.out.println(obj);
-		
-		//Seller obj1 = new Seller(1,"Gustavo", "gustavo@gmail.com", sdf.parse("24/03/1983"), 2000.0, obj);
+		//SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
-		System.out.println(sellerDao.toString());
+		Seller seller = sellerDao.findById(3);
+		
+		System.out.println(seller);
 
 	}
 
--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,11 +1,12 @@
 package model.dao;
 
+import db.DB;
 import model.dao.impl.SellerDaoJDBC;
 
 public class DaoFactory {
 	/*Classe aux. terá oprações estáticas para instanciar os Daos*/
 	
 	public static SellerDao createSellerDao() {
-		return new SellerDaoJDBC();
+		return new SellerDaoJDBC(DB.getConnection());
 	}
 }
